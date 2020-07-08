ed2fbbb3d11d767bee48cd081e8d378f5a61e2d0
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-08 16:44:54 -0500

    Create model, migration, factory and controller; start test value in seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        App\User::create([
+            'name' => 'Alejandro Cataño H',
+            'email' => 'a@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
