06fa649d3b663118fe81ed728040a5f0af641a7f
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-08 15:43:58 -0500

    apply slug to URL

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
 }
