6eaaa556de1997ec5204ab41430246c51857abe9
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-08 22:28:25 -0500

    Created views and PageController

--- app/Post.php
+++ app/Post.php
@@ -23,4 +23,9 @@ class Post extends Model
             ]
         ];
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@posts');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/blog/{post}', 'PageController@post')->name('post');
 
 Auth::routes();
 
